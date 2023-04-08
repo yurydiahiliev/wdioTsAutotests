40fcf79262316c39d58c87bdbd87f2332a6b28ec
Diahiliev, Yurii <Yurii.Diahiliev@McKesson.com>
2023-04-08 19:07:36 +0300

    added logout test

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,14 +1,17 @@
 import LoginPage from '../../login.page.ts'
 import SecurePage from '../../secure.page.ts'
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Login/logout tests', () => {
+    it('User should login with valid credentials to app', async () => {
         await LoginPage.open()
         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
         await expect(SecurePage.flashAlert).toBeExisting()
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!')
     })
+
+    it('User should be logged out from app', async () => {
+        await SecurePage.logout()
+        await expect(LoginPage.btnSubmit).toBeDisplayed()
+    })
 })
-
-
